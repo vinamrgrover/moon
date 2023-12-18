62eedf3f42b4f9c7ded8014e6e1a995808015172
Vinamr Grover <vinamrgrover@gmail.com>
2023-12-18 11:10:54 +0530

    Added connection string to _attach_postgres() method and added some exceptions

--- moon/lander.py
+++ moon/lander.py
@@ -1,8 +1,20 @@
+"""
+This module demonstrates the usage of Moon Module.
+
+Classes:
+    MoonBase
+    MoonQuery
+"""
+
 import duckdb
-from duckdb import IOException
+from duckdb import IOException, BinderException, ParserException
+import pandas as pd
 
 
 class MoonBase:
+    """
+    Super class for this module.
+    """
 
     @staticmethod
     def _create_connection() -> duckdb.DuckDBPyConnection:
@@ -11,27 +23,65 @@ class MoonBase:
 
 
 class MoonQuery(MoonBase):
-    def __init__(self, host: str = "localhost", db: str = "postgres", user : str = "postgres", password : str = None):
+    def __init__(
+        self,
+        host: str,
+        db: str,
+        user: str,
+        password: str,
+    ):
         super().__init__()
         self.host = host
         self.port = 5432
-        self.db = db
+        self.dbname = db
         self.user = user
         self.password = password
-        self.cols = []
-        self.con = self._create_connection()
+        self._cols = []
+        self._con = self._create_connection()
+        self._attach_postgres()
 
     def _attach_postgres(self):
-
+        conn_str = " ".join(
+            [
+                f"{key}={val}"
+                for key, val in vars(self).items()
+                if type(val) == str or type(val) == int
+            ]
+        )
         try:
-            self.con.sql(
+            self._con.sql(
                 f"""
-            ATTACH 'host = {self.host} port = {self.port} dbname = {self.db} user = {self.user} password = {self.password}' AS psql (TYPE postgres);
+            ATTACH '{conn_str}' AS psql (TYPE postgres);
             """
             )
-            self.con.sql("USE psql;")
-        except IOException as e:
-            raise IOException
+            self._con.sql("USE psql;")
 
-    def execute(self, query : str):
-        return self.con.sql(query)
+        # If PostgreSQL client is unavailable
+        except IOException as e:
+            self.con = None
+            raise e
+
+        # If DB is already attached
+        except BinderException:
+            self._con.sql(
+                """
+            USE memory;
+            DETACH psql;
+            """
+            )
+
+            self._attach_postgres()
+
+    def execute(
+        self, query: str, df: bool = None
+    ) -> (pd.DataFrame, duckdb.DuckDBPyRelation):
+        if df:
+            try:
+                return self.con.sql(query).df()
+            except ParserException as e:
+                raise e
+
+        try:
+            return self._con.sql(query)
+        except ParserException as e:
+            raise e
