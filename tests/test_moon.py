88fb1d52176f8c311447be9896d3944030f0ef9b
Vinamr Grover <vinamrgrover@gmail.com>
2023-12-24 21:33:29 +0530

    Updated tests for test_query() method

--- tests/test_moon.py
+++ tests/test_moon.py
@@ -1,6 +1,8 @@
 import unittest
 from moon import MoonBase, MoonQuery
 import pytest
+from testcontainers.postgres import PostgresContainer
+import duckdb
 
 
 class test_moon_base(unittest.TestCase):
@@ -27,12 +29,18 @@ class test_moon_query(unittest.TestCase):
 
 class test_query(unittest.TestCase):
 
-    # @classmethod
-    # def setUpClass(cls):
-    #     cls.mq = MoonQuery(db = 'postgres', user = 'moonuser')
-    #     cls.mq._attach_postgres()
+    @classmethod
+    def setUp(cls):
+        cls.postgres_container = PostgresContainer()
+        cls.postgres_container.with_exposed_ports(5432)
+        cls.postgres_container.start()
+        container_port = cls.postgres_container.get_exposed_port(port = 5432)
+        cls.mq = MoonQuery(db = 'test', user = 'test', password = 'test', host = 'localhost', port = container_port)
+        cls.mq._attach_postgres()
 
-    @pytest.mark.skip('ignoring test')
+    def tearDown(self):
+        self.postgres_container.stop()
+
     def test_query(self):
         res = self.mq.execute("SHOW TABLES;")
-        print(res)
+        self.assertEqual(type(res), duckdb.DuckDBPyRelation)
